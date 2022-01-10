21c76952df3522ae1168a617d9173a7d9c027bbe
John Costa <johncosta027@gmaiil.com>
2022-01-10 13:18:31 +0000

    Added a comment template for files.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+//======================================================
+//
+//  Index file
+//
+//  This is the starting point of the project, it starts
+//  the express server and integrates all the different
+//  routers that are needed.
+//
+//======================================================
+
 import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
