263c4872b251529ede6de3ac23ea1a2479304625
John Costa <johncosta027@gmail.com>
2022-09-17 23:06:25 +0100

    feat: added a proper mutation

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer, gql } from "apollo-server-express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from ".prisma/client";
+import { AddStudentInput } from "@lessonlog/graphql-types";
 
 const app = express();
 const prisma = new PrismaClient();
@@ -8,21 +9,25 @@ const prisma = new PrismaClient();
 const typeDefs = gql`
   type Student {
     name: String!
+    startDate: Float!
   }
   type Query {
     students: [Student]
   }
 
   type Mutation {
-    addStudent(name: String!): Student
+    addStudent(name: String!, startDate: Float!): Student
   }
 `;
 
 const resolvers = {
   Query: {
-    async students(): Promise<{ name: string }[]> {
+    async students(): Promise<{ name: string; startDate: number }[]> {
       const students = await prisma.students.findMany();
-      const returnStudents = students.map((s) => ({ name: s.name }));
+      const returnStudents = students.map((s) => ({
+        name: s.name,
+        startDate: s.startDate.getTime(),
+      }));
 
       return returnStudents;
     },
@@ -30,15 +35,19 @@ const resolvers = {
   Mutation: {
     async addStudent(
       _: unknown,
-      { name }: { name: string }
-    ): Promise<{ name: string }> {
-      console.log(name);
-      const newStudent = await prisma.students.create({
-        data: {
-          name: name,
-        },
-      });
-      return newStudent;
+      { name, startDate }: AddStudentInput
+    ): Promise<{ name: string; startDate: Date } | undefined> {
+      try {
+        const newStudent = await prisma.students.create({
+          data: {
+            name: name,
+            startDate: new Date(startDate),
+          },
+        });
+        return newStudent;
+      } catch (e) {
+        return undefined;
+      }
     },
   },
 };
