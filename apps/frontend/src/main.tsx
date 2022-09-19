847234d84188f0f276a5ba59f0ccc13bba84cbb6
John Costa <johncosta027@gmail.com>
2022-09-19 19:53:15 +0100

    adding lesson mutation

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -20,6 +20,13 @@ const studentQuery = gql`
     students {
       name
       startDate
+      id
+      lessons {
+        id
+        studentId
+        price
+        summary
+      }
     }
   }
 `;
@@ -33,19 +40,56 @@ const studentMutation = gql`
   }
 `;
 
-interface Student {
-  name: string;
-  startDate: number;
+const lessonMutation = gql`
+  mutation addLesson(
+    $studentId: String!
+    $date: Float!
+    $price: Float!
+    $summary: String
+  ) {
+    addLesson(
+      studentId: $studentId
+      date: $date
+      price: $price
+      summary: $summary
+    ) {
+      id
+      studentId
+      date
+      price
+      summary
+    }
+  }
+`;
+
+interface LessonsQuery {
+  id: string;
+  studentId: string;
+  price: number;
+  summary?: string;
 }
+
+interface StudentsQuery {
+  id: string;
+  name: string;
+  startDate: Date;
+  lessons: Array<LessonsQuery>;
+}
+
 interface StudentData {
-  students: Student[];
+  students: StudentsQuery[];
 }
 
 function App() {
   const [name, setName] = createSignal("");
   const [date, setDate] = createSignal(new Date().getTime());
 
+  const [student, setStudent] = createSignal("");
+  const [lessonTime, setLessonTime] = createSignal(new Date().getTime());
+  const [summary, setSummary] = createSignal("");
+
   const data = createQuery<StudentData>(studentQuery);
+
   const [mutate] = createMutation(studentMutation, {
     refetchQueries: [
       {
@@ -53,39 +97,102 @@ function App() {
       },
     ],
   });
+  const [mutateLesson] = createMutation(lessonMutation, {
+    refetchQueries: [
+      {
+        query: studentQuery,
+      },
+    ],
+  });
+
   return (
-    <>
-      <input
-        type="text"
-        placeholder="Type here"
-        class="input input-primary w-full max-w-xs"
-        onChange={(e) => setName(e.currentTarget.value)}
-      />
-      <input
-        type="date"
-        placeholder="Joining date"
-        class="input input-primary w-full max-w-xs"
-        onChange={(e) => setDate(new Date(e.currentTarget.value).getTime())}
-      />
-      <button
-        class="btn btn-primary"
-        onClick={() => {
-          mutate({
-            variables: { name: name(), startDate: date() },
-          });
-        }}
-      >
-        Create student
-      </button>
-      <For each={data()?.students}>
-        {(student) => (
-          <div class="flex gap-4">
-            <p>{student.name}</p>
-            <p>{new Date(student.startDate).toISOString().slice(0, 10)}</p>
-          </div>
-        )}
-      </For>
-    </>
+    <div class="flex flex-col">
+      <div>
+        <input
+          type="text"
+          placeholder="Type here"
+          class="input input-secondary w-full max-w-xs"
+          onChange={(e) => setName(e.currentTarget.value)}
+        />
+        <input
+          type="date"
+          placeholder="Joining date"
+          class="input input-secondary w-full max-w-xs"
+          onChange={(e) => setDate(new Date(e.currentTarget.value).getTime())}
+        />
+        <button
+          class="btn btn-primary"
+          onClick={() => {
+            mutate({
+              variables: { name: name(), startDate: date() },
+            });
+          }}
+        >
+          Create student
+        </button>
+        <For each={data()?.students}>
+          {(student) => (
+            <div class="flex gap-4 w-96">
+              <div class="w-full">
+                <p>{student.name}</p>
+              </div>
+              <div class="w-full">
+                <p>{new Date(student.startDate).toISOString().slice(0, 10)}</p>
+              </div>
+
+              <div class="flex flex-col">
+                <For each={student.lessons}>
+                  {(lesson) => <p>{lesson.id}</p>}
+                </For>
+              </div>
+            </div>
+          )}
+        </For>
+      </div>
+      <div>
+        <select
+          class="select select-secondary w-full max-w-xs"
+          onChange={(e) => {
+            setStudent(e.currentTarget.value);
+          }}
+        >
+          <option>---</option>
+          <For each={data()?.students}>
+            {(student) => {
+              return <option value={student.id}>{student.name}</option>;
+            }}
+          </For>
+        </select>
+        <input
+          type="text"
+          class="input input-secondary"
+          onChange={(e) => setSummary(e.currentTarget.value)}
+        />
+        <input
+          type="date"
+          class="input input-secondary"
+          onChange={(e) =>
+            setLessonTime(new Date(e.currentTarget.value).getTime())
+          }
+        />
+        <button
+          class="btn btn-primary"
+          onClick={() => {
+            console.log(student());
+            mutateLesson({
+              variables: {
+                studentId: student(),
+                date: lessonTime(),
+                price: 40,
+                summary: summary(),
+              },
+            });
+          }}
+        >
+          Create student
+        </button>
+      </div>
+    </div>
   );
 }
 
