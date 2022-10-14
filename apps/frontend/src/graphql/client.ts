9b473ef3049b063f599fd32c67222af91add2c53
John Costa <johncosta027@gmail.com>
2022-10-14 14:44:59 +0100

    added frontend env variables

--- apps/frontend/src/graphql/client.ts
+++ apps/frontend/src/graphql/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@merged/solid-apollo";
 
 export const client = new ApolloClient({
-  uri: `http://${window.location.hostname}:3030`,
+  uri: `http://${window.location.hostname}${import.meta.env.VITE_BACKEND_URL}`,
   cache: new InMemoryCache(),
 });
