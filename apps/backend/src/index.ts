4b43b30e614343c9b3de896a2f7da9bda216ef73
John Costa <johncosta027@gmail.com>
2022-10-26 18:14:25 +0100

    adding authentication ish

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,8 @@
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { typeDefs, resolvers } from "./graphql";
+import express, { NextFunction, Request, Response } from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './graphql';
+// @ts-ignore
+import cors from 'cors';
 
 const app = express();
 
@@ -9,10 +11,20 @@ const server = new ApolloServer({
   resolvers,
 });
 
+app.use(cors());
+
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  if (req.headers['verycoolheadernothingtoseehere'] === process.env.PASSWORD) {
+    next();
+  } else {
+    res.json({ error: 'Not authorised' });
+  }
+});
+
 server.start().then(() => {
-  server.applyMiddleware({ app, path: "/", cors: true });
+  server.applyMiddleware({ app, path: '/', cors: true });
 });
 
 app.listen(3030, () => {
-  console.log("Server started");
+  console.log('Server started');
 });
--- apps/frontend/src/graphql/client.ts
+++ apps/frontend/src/graphql/client.ts
@@ -3,4 +3,7 @@ import { ApolloClient, InMemoryCache } from "@merged/solid-apollo";
 export const client = new ApolloClient({
   uri: `http://${window.location.hostname}${import.meta.env.VITE_BACKEND_URL}`,
   cache: new InMemoryCache(),
+  headers: {
+    verycoolheadernothingtoseehere: localStorage.getItem("lessonlog-key") || "",
+  },
 });
