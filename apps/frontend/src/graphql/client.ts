f8c2a0fed15fa3a34ab61b697e0804436cb26577
Ubuntu <John@JohnUK.sn2bzbxh2jyezjejbjgyhgvdsb.zx.internal.cloudapp.net>
2023-03-10 22:51:42 +0000

    FIX: Using protocol

--- apps/frontend/src/graphql/client.ts
+++ apps/frontend/src/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@merged/solid-apollo";
 
 export const client = new ApolloClient({
-  uri: `http://${window.location.hostname}${import.meta.env.VITE_BACKEND_URL}`,
+  uri: `${location.protocol}//${window.location.hostname}${import.meta.env.VITE_BACKEND_URL}`,
   cache: new InMemoryCache(),
   headers: {
     verycoolheadernothingtoseehere: localStorage.getItem("lessonlog-key") || "",
